cc12aa2b2655e0f0429d88c685cdee9a04030087
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-14 13:41:51 +0000

    some brief CSS added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import DataFetch from './components/DataFetch';
 import NavBar from './components/NavBar';
 import EmailForm from './components/EmailForm';
